922da0b0569ac2fa5793e685ed7d34f51aa40209
Kevin Kokal <kkokal@gmail.com>
2014-03-13 21:02:11 -0500

    Added second controller method

--- src/main/java/com/kjk/HelloController.java
+++ src/main/java/com/kjk/HelloController.java
@@ -2,15 +2,28 @@ package com.kjk;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
-@RequestMapping("/")
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
 		return "hello";
 	}
+
+    @ResponseBody
+    @RequestMapping(value="/api/data/{name}", method = RequestMethod.GET)
+    public Map<String, String> getData(@PathVariable String name) {
+        Map<String, String> m = new HashMap<String, String>();
+        m.put("id", "12345");
+        m.put("name", name);
+        return m;
+    }
 }
